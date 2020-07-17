54ab486be1ee9cc68629dc51960e684dc557fe19
Diego Gennaro <kdc.crow@gmail.com>
2020-07-16 22:31:03 -0300

    first test with twitter timeline

--- pages/api/timeline/index.tsx
+++ pages/api/timeline/index.tsx
@@ -5,7 +5,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     var client = new Twitter({
         consumer_key: process.env.TW_CONSUMER_KEY,
         consumer_secret: process.env.TW_CONSUMER_SECRET,
-        access_token_key: process.env.TW_ACCESS_TOKEN__KEY,
+        access_token_key: process.env.TW_ACCESS_TOKEN_KEY,
         access_token_secret: process.env.TW_ACCESS_TOKEN_SECRET
     });
 
